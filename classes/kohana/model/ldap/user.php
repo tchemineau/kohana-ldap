bc52eb35ec0af2ce2a2df3996faab9e67da49f6a
Thomas Chemineau <thomas.chemineau@gmail.com>
2012-07-26 16:37:39 +0200

    auth module now use ldap_user model

---
+++ classes/kohana/model/ldap/user.php
@@ -0,0 +1,119 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * LDAP user model.
+ *
+ * @package   Kohana/LDAP
+ * @author    Thomas Chemineau - thomas.chemineau@gmail.com
+ * @copyright (c) 2012 Thomas Chemineau
+ * @license   http://kohanaframework.org/license
+ */
+class Kohana_Model_Ldap_User extends Model
+{
+
+	/**
+	 * LDAP user data.
+	 *
+	 * @var array
+	 */
+	private $_data = null;
+
+	/**
+	 * LDAP database instance.
+	 *
+	 * @var Database
+	 */
+	private $_database = null;
+
+	/**
+	 * Authenticate a user against an LDAP directory.
+	 *
+	 * @param   string   $password
+	 * @return  boolean
+	 */
+	public function authenticate ( $password )
+	{
+		if (is_null($this->_data) || !isset($this->_data['dn']))
+		{
+			return FALSE;
+		}
+
+		return $this->_database->bind($this->_data['dn'], $password);
+	}
+
+	/**
+	 * Get or set user data.
+	 *
+	 * @param   array   $data
+	 * @return  array
+	 */
+	public function data ( $data = null )
+	{
+		if (is_null($data))
+		{
+			return $this->_data;
+		}
+		$this->_data = $data;
+		return $this;
+	}
+
+	/**
+	 * Get or set database instance.
+	 *
+	 * @param   Database   $database
+	 * @return  Database
+	 * @return  Model_Ldap_User
+	 */
+	public function database ( $database = null )
+	{
+		if (is_null($database))
+		{
+			return $this->_database;
+		}
+		$this->_database = $database;
+		return $this;
+	}
+
+	/**
+	 * Get a user from its username.
+	 *
+	 * @return Model_Ldap_User
+	 */
+	public function get ( $username )
+	{
+		$config = $this->_database->config();
+
+		$query = array (
+			'filter' => $this->_database->format_filter($config['search']['user']['filter'], array('u' => $username)),
+			'basedn' => $config['search']['user']['basedn'],
+			'scope'  => $config['search']['user']['scope'],
+			'attributes' => $config['mapping']['user']
+		);
+
+		$result = $this->_database->query(Database::SELECT, $query);
+
+		if (is_array($result))
+		{
+			$keys = array_keys($result);
+			$ldapdata = $result[$keys[0]];
+
+			$data = array(
+				'dn' => $ldapdata['dn'],
+			);
+			foreach ($query['attributes'] as $var => $attr)
+			{
+				if (isset($ldapdata[$attr]))
+				{
+					$data[$var] = $ldapdata[$attr];
+				}
+			}
+
+			$user = new self();
+			return $user->database($this->_database)->data($data);
+		}
+
+		return FALSE;
+	}
+
+} // End Model_Ldap_User
+
