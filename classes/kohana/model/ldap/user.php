a562cd5ff03ea9bdc38be92dce8f10a9814d7e3b
Thomas Chemineau <thomas.chemineau@gmail.com>
2012-07-26 17:32:38 +0200

    possibility to retrieve a user by its DN, and allow to retrieve attributes

--- classes/kohana/model/ldap/user.php
+++ classes/kohana/model/ldap/user.php
@@ -42,6 +42,63 @@ class Kohana_Model_Ldap_User extends Model
 	}
 
 	/**
+	 * Return user data attributes from LDAP directory.
+	 *
+	 * @param   array   $attributes
+	 * @return  array
+	 * @return  false
+	 */
+	public function attribute ( $attributes )
+	{
+		if (is_null($this->_database))
+		{
+			return FALSE;
+		}
+		if (!is_null($attributes) && !is_array($attributes))
+		{
+			$attributes = array($attributes);
+		}
+		if (sizeof($attributes) == 0)
+		{
+			return array();
+		}
+
+		$result = $this->_database->query(
+			Database::SELECT,
+			array (
+				'filter' => '(objectClass=top)',
+				'basedn' => $this->_data['_dn'],
+				'scope'  => 'base',
+				'attributes' => $attributes
+			)
+		);
+
+		if (is_array($result))
+		{
+			$keys = array_keys($result);
+			$ldapdata = $result[$keys[0]];
+
+			$data = array(
+				'_dn' => $ldapdata['dn'],
+				'_type' => 'ldap',
+				'_name' => $this->_database->get_name()
+			);
+
+			foreach ($attributes as $var => $attr)
+			{
+				if (isset($ldapdata[$attr]))
+				{
+					$data[$var] = $ldapdata[$attr];
+				}
+			}
+
+			return $data;
+		}
+
+		return FALSE;
+	}
+
+	/**
 	 * Get or set user data.
 	 *
 	 * @param   array   $data
@@ -84,18 +141,36 @@ class Kohana_Model_Ldap_User extends Model
 	/**
 	 * Get a user from its username.
 	 *
+	 * @param  string   $username
+	 * @param  boolean  $dn
 	 * @return Model_Ldap_User
 	 */
-	public function get ( $username )
+	public function get ( $username, $dn = false )
 	{
 		$config = $this->_database->get_config();
 
-		$query = array (
-			'filter' => $this->_database->format_filter($config['search']['user']['filter'], array('u' => $username)),
-			'basedn' => $config['search']['user']['basedn'],
-			'scope'  => $config['search']['user']['scope'],
-			'attributes' => $config['mapping']['user']
-		);
+		if ($dn)
+		{
+			$query = array (
+				'filter' => '(objectClass=top)',
+				'basedn' => $username,
+				'scope'  => 'base',
+				'attributes' => $config['mapping']['user']
+			);
+		}
+		else
+		{
+			$filter = $this->_database->format_filter(
+				$config['search']['user']['filter'],
+				array('u' => $username)
+			);
+			$query = array (
+				'filter' => $filter,
+				'basedn' => $config['search']['user']['basedn'],
+				'scope'  => $config['search']['user']['scope'],
+				'attributes' => $config['mapping']['user']
+			);
+		}
 
 		$result = $this->_database->query(Database::SELECT, $query);
 
